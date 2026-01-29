73eea4f1904e828cdfed701c76c6597331d74368
Tannu Kumari <121978242+tannu371@users.noreply.github.com>
2026-01-29 23:10:04 +0530

    Add main.py and pythonanywhere.py for weather alert functionality; include requirements.txt

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import requests
+from twilio.rest import Client
+from dotenv import load_dotenv
+load_dotenv() 
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+
+weather_params = {
+    "lat": 46.947975,
+    "lon": 7.447447,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+# print(weather_data["list"][0]["weather"][0]["id"])
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_=os.environ.get("TWILIO_PHONE_NUMBER"),
+        to=os.environ.get("RECIPIENT_PHONE_NUMBER")
+    )
+    print(message.status)
